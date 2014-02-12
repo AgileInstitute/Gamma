f023c146de6346e0bdaf991769f580dc1b58933a
arjunseshadri <arjun.seshadri@gmail.com>
2014-02-12 11:11:51 -0600

    Auction class

--- src/main/Auction.java
+++ src/main/Auction.java
@@ -1,1 +1,19 @@
 package main;
+
+public class Auction
+{
+	String _userName;
+	
+	public Auction(String userName)
+	{
+		_userName = userName;
+	}
+	
+	public boolean IsValidBidder(String bidder)
+	{
+		if (bidder == null)
+			return false;
+		return _userName.equalsIgnoreCase(bidder);
+	}
+	
+}
