804b04ca0f30eff1a6d6010f44256e441a23fbd4
Alex Street <alexstreet@gmail.com>
2014-02-13 11:44:18 -0600

    adding bids tests and logic

--- src/main/java/Auction/Bids.java
+++ src/main/java/Auction/Bids.java
@@ -3,19 +3,34 @@ package Auction;
 import java.util.HashMap;
 
 public class Bids {
-	HashMap<String, Bid> bids = new HashMap<String, Bid>();
-	Bid highBid = new Bid(null, 0);
+	HashMap<String, Bid> bids;
+	Bid highBid;
+	
+	public Bids()
+	{
+		bids = new HashMap<String, Bid>();
+		highBid = new Bid(null, 0);
+	}
 	
 	public HashMap<String, Bid> getBids() {
 		return bids;
 	}
 	
-	public void addBid(Bid bid) {
-		this.bids.put(bid.get_user(), bid);
+	public boolean addBid(Bid bid) {
+		if (bid.get_bid() <= highBid.get_bid())
+		{
+			return false;
+		}
+		else
+		{
+			bids.put(bid.get_user(), bid);
+			highBid = bid;
+			return true;
+		}
 	}
 	
 	public void removeBid(Bid bid){
-		this.bids.remove(bid);
+		bids.remove(bid);
 	}
 	
 	public Bid getHighBid() {
@@ -25,5 +40,5 @@ public class Bids {
 	public void setHighBid(Bid highBid) {
 		this.highBid = highBid;
 	}
-
+	
 }
